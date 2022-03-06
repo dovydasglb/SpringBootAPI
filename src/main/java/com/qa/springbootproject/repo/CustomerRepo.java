2cee95129121d3b8b0d5f458f6ba7a52a0ba9053
dovydasglb <dovydasglb@gmail.com>
2022-03-06 23:22:45 +0000

    Completed customer repository

--- src/main/java/com/qa/springbootproject/repo/CustomerRepo.java
+++ src/main/java/com/qa/springbootproject/repo/CustomerRepo.java
@@ -1,5 +1,11 @@
 package com.qa.springbootproject.repo;
 
-public interface CustomerRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.springbootproject.domain.Customer;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
 
 }
