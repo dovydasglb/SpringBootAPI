93ee124318d28f6c2cb593429f10d59fe384acee
dovydasglb <dovydasglb@gmail.com>
2022-03-07 11:31:42 +0000

    Completed service layer for customer class

--- src/main/java/com/qa/springbootproject/service/CustomerService.java
+++ src/main/java/com/qa/springbootproject/service/CustomerService.java
@@ -1,46 +1,57 @@
 package com.qa.springbootproject.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qa.springbootproject.domain.Customer;
 import com.qa.springbootproject.repo.CustomerRepo;
 
 @Service
-public class CustomerService implements ServiceInterface<Customer>{
+public class CustomerService implements ServiceInterface<Customer> {
 
 	private CustomerRepo repo;
 
 	// Dependency injection
+	@Autowired
 	public CustomerService(CustomerRepo repo) {
 		super();
 		this.repo = repo;
 	}
 
 	public Customer create(Customer body) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.repo.save(body);
 	}
 
 	public List<Customer> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.repo.findAll();
 	}
 
 	public Customer readById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.repo.findById(id).orElseThrow();
 	}
 
 	public Customer updateById(Long id, Customer body) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Customer> existingRecord = this.repo.findById(id);
+		Customer updatedRecord = existingRecord.get();
+		updatedRecord.setFirstName(body.getFirstName());
+		updatedRecord.setLastName(body.getLastName());
+		updatedRecord.setEmailAddress(body.getEmailAddress());
+		updatedRecord.setPostCode(body.getPostCode());
+
+		return this.repo.save(updatedRecord);
 	}
 
 	public boolean deleteById(Long id) {
-		// TODO Auto-generated method stub
-		return false;
+		Optional<Customer> existingRecord = this.repo.findById(id);
+
+		if (existingRecord.isPresent()) {
+			this.repo.deleteById(id);
+			return !(this.repo.existsById(id));
+		} else
+			return false;
 	}
-	
+
 }
