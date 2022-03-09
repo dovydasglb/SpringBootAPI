bf44c9ded168b6a5ce8eb0972c3b5fc98a5b1ee3
dovydasglb <dovydasglb@gmail.com>
2022-03-09 21:15:15 +0000

    Added lombok and refactored domain class to use lombok annotation

--- src/main/java/com/qa/springbootproject/domain/Customer.java
+++ src/main/java/com/qa/springbootproject/domain/Customer.java
@@ -1,7 +1,5 @@
 package com.qa.springbootproject.domain;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,7 +11,20 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 @Table(name = "customer")
 public class Customer {
 
@@ -47,90 +58,12 @@ public class Customer {
 	@Pattern(regexp = "^[A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][ABD-HJLNP-UW-Z]{2}$", message = "Invalid post code")
 	private String postCode;
 
-	public Customer() {
-		super();
-	}
-
 	public Customer(String firstName, String lastName, String emailAddress, String postCode) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.emailAddress = emailAddress;
 		this.postCode = postCode;
-	}
-
-	public Customer(long id, String firstName, String lastName, String emailAddress, String postCode) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.emailAddress = emailAddress;
-		this.postCode = postCode;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmailAddress() {
-		return emailAddress;
-	}
-
-	public void setEmailAddress(String emailAddress) {
-		this.emailAddress = emailAddress;
-	}
-
-	public String getPostCode() {
-		return postCode;
-	}
-
-	public void setPostCode(String postCode) {
-		this.postCode = postCode;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(emailAddress, firstName, id, lastName, postCode);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Customer other = (Customer) obj;
-		return Objects.equals(emailAddress, other.emailAddress) && Objects.equals(firstName, other.firstName)
-				&& id == other.id && Objects.equals(lastName, other.lastName)
-				&& Objects.equals(postCode, other.postCode);
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailAddress="
-				+ emailAddress + ", postCode=" + postCode + "]";
 	}
 
 }
